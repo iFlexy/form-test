e56a2a99556ecdb638238812ef5ba8c5982c9361
iFlexy <107810874+iFlexy@users.noreply.github.com>
2022-06-21 00:53:43 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -1,47 +1,35 @@
 // The default code is a module class (inherited from xui.Module)
 // Ensure that all the value of "key/value pair" does not refer to external variables
-xui.Class('App', 'xui.Module',{
+Class.destroy('App');
+Class('App', 'xui.Module',{
     Instance:{
-        // Dependency classes
-        Dependencies:[],
-        // Required modules
-        Required:[],
-
-        // To initialize properties
-        properties : {},
-
-        // To initialize instance(e.g. properties)
-        initialize : function(){
-        },
-
-        // To initialize internal components (mostly UI controls)
-        // *** If you're not a skilled, dont modify this function manually ***
-        iniComponents : function(){
-            // [[Code created by CrossUI RAD Studio
-            var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+        iniComponents:function(){
+            // [[code created by CrossUIUI Builder
+            var host=this, children=[], append=function(child){children.push(child.get(0));
+                                                            };
+            append((newxui.UI.SLabel)
+                   .setHost(host,"slabel1").setLeft(80).setTop(60).setWidth(44).setCaption("Name:") );
+            append((newxui.UI.SLabel)
+                   .setHost(host,"slabel2").setLeft(80).setTop(90).setCaption("Age:").setWidth(44));
+            append((newxui.UI.Input)
+                   .setHost(host,"iName").setLeft(130).setTop(60).setValueFormat("[^.*]").setValue("Jack"));
+            append((newxui.UI.ComboInput)
+                   .setHost(host,"iAge").setLeft(130).setTop(90).setType("spin").setIncrement(1).setMin(20).setMax(60).setValue("35"));
+            append((newxui.UI.SCheckBox)
+                   .setHost(host,"cFull").setLeft(130).setTop(130).setCaption("Full time"));
+            append((newxui.UI.SButton).setHost(host,"submit")
+                   .setLeft(130).setTop(170).setCaption("SUBMIT").onClick("_submit_onclick"));
             return children;
-            // ]]Code created by CrossUI RAD Studio
+            // ]]code created by CrossUIUI Builder
         },
-
-        // Give a chance to determine which UI controls will be appended to parent container
-        customAppend : function(parent, subId, left, top){
-            // "return false" will cause all the internal UI controls will be added to the parent panel
-            return false;
-        }
-        /*,
-        // To determine how properties affects this module
-        propSetAction : function(prop){
-        },
-        // To set all node's style in this modlue
-        customStyle:{}
-    },
-    //To customize the default properties and event handlers
-    Static:{
-        $DataModel:{
-        },
-        $EventHandlers:{
-        }
-    */
+        _submit_onclick:function(profile, e, src, value) {
+            if(!this.iName.checkValid()){
+                xui.alert('You must specify Name');
+                return;
+            }
+            var name=this.iName.updateValue().getValue(),age=this.iAge.updateValue().getValue(),
+                full=this.cFull.updateValue().getValue(); 
+        }     
     }
 });
+            
